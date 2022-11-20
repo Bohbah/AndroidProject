1277d9f37cbaac751a83d02027e27822d935ab89
TNBerkman <90403029+TNBerkman@users.noreply.github.com>
2022-11-20 14:50:34 -0500

    Bottom Navigation Bar finished

--- app/src/main/java/com/androidstudioprojects/grapevine/MainActivity.kt
+++ app/src/main/java/com/androidstudioprojects/grapevine/MainActivity.kt
@@ -3,6 +3,9 @@ package com.androidstudioprojects.grapevine
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import com.androidstudioprojects.grapevine.fragments.*
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
@@ -13,28 +16,35 @@ class MainActivity : AppCompatActivity() {
         /*
         Bottom Nav Bar navigation code
          */
+        val fragmentManager: FragmentManager = supportFragmentManager
         findViewById<BottomNavigationView>(R.id.bottom_navigation).setOnItemSelectedListener {
             item ->
-
+            var fragmentToShow : Fragment? = null
             when(item.itemId) {
                 R.id.action_home -> {
-                    //TODO Navigate to home/feed fragment
+                    fragmentToShow = HomeFragment()
                 }
                 R.id.action_calendar -> {
-                    //TODO Navigate to calendar fragment
+                    fragmentToShow = CalendarFragment()
                 }
                 R.id.action_compose -> {
-                    //TODO Navigate to compose fragment
+                    fragmentToShow = ComposeFragment()
                 }
                 R.id.action_chat -> {
-                    //TODO Navigate to chat fragment
+                    fragmentToShow = ChatFragment()
                 }
                 R.id.action_profile -> {
-                    //TODO Navigate to profile fragment
+                    fragmentToShow = ProfileFragment()
                 }
+            }
+
+            if (fragmentToShow != null){
+                fragmentManager.beginTransaction().replace(R.id.flContainer, fragmentToShow).commit()
             }
 
             true //Not sure what this does but its necessary I promise
         }
+        // Set default view
+        findViewById<BottomNavigationView>(R.id.bottom_navigation).selectedItemId = R.id.action_home
     }
 }
