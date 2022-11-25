a592ad123a200a2b52a34903c85d3945dab1b14d
mflynn19 <meghan.flynn@theflynn.org>
2022-11-25 12:34:41 -0500

    [Meghan Flynn] Parse connection setup

--- app/src/main/java/com/androidstudioprojects/grapevine/LoginActivity.kt
+++ app/src/main/java/com/androidstudioprojects/grapevine/LoginActivity.kt
@@ -2,8 +2,10 @@ package com.androidstudioprojects.grapevine
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
+import com.parse.ParseObject
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,7 +17,18 @@ class LoginActivity : AppCompatActivity() {
         val password = findViewById<EditText>(R.id.etPassword).text.toString()
         loginUser(username, password)
         }
+
+        /*val firstObject = ParseObject("FirstClass")
+        firstObject.put("message","Hey ! First message from android. Parse is now connected")
+        firstObject.saveInBackground {
+            if (it != null){
+                it.localizedMessage?.let { message -> Log.e("MainActivity", message) }
+            }else{
+                Log.d("MainActivity","Object saved.")
+            }
+        }*/
     }
+
 
     private fun loginUser(username: String, password: String) {
 
