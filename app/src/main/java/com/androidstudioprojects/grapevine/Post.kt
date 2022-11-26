21fdcbb4ffe8a4216c1a9228ec9b8c79c9d645bc
TNBerkman <90403029+TNBerkman@users.noreply.github.com>
2022-11-26 16:45:02 -0500

    Post Viewing with error?

--- app/src/main/java/com/androidstudioprojects/grapevine/Post.kt
+++ app/src/main/java/com/androidstudioprojects/grapevine/Post.kt
@@ -1,4 +1,40 @@
 package com.androidstudioprojects.grapevine
 
-class Post {
+import com.parse.*
+
+
+// Description : String
+// Image : File
+// User : User
+@ParseClassName("Post")
+class Post : ParseObject() {
+
+    // getters and setters
+    fun getDescription(): String? {
+        return getString(KEY_DESCRIPTION)
+    }
+    fun setDescription(description: String) {
+        put(KEY_DESCRIPTION, description)
+    }
+
+    fun getPFP(): ParseFile? {
+        return getParseFile(KEY_PFP)
+    }
+    fun setPFP(parsefile: ParseFile) {
+        put(KEY_PFP, parsefile)
+    }
+
+    fun getUser(): ParseUser? {
+        return getParseUser(KEY_USER)
+    }
+    fun setUser(user: ParseUser) {
+        put(KEY_USER, user)
+    }
+
+    // Keys
+    companion object {
+        const val KEY_DESCRIPTION = "Description"
+        const val KEY_PFP = "Image"
+        const val KEY_USER = "User"
+    }
 }
---
+++ app/src/main/java/com/androidstudioprojects/grapevine/PostAdapter.kt
@@ -0,0 +1,51 @@
+package com.androidstudioprojects.grapevine
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+
+class PostAdapter(val context: Context, val posts: List<Post>)
+    : RecyclerView.Adapter<PostAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PostAdapter.ViewHolder {
+        val view = LayoutInflater.from(context).inflate(R.layout.item_post, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: PostAdapter.ViewHolder, position: Int) {
+        val post = posts.get(position)
+        holder.bind(post)
+    }
+
+    override fun getItemCount(): Int {
+        return posts.size
+    }
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val ivPFP: ImageView
+        //TODO Org
+        val tvUsername: TextView
+        val tvDescription: TextView
+
+        init {
+            ivPFP = itemView.findViewById(R.id.ivPFP)
+            //TODO Org
+            tvUsername = itemView.findViewById(R.id.tvUsername)
+            tvDescription = itemView.findViewById(R.id.tvDescription)
+        }
+
+        fun bind(post: Post) {
+            tvDescription.text = post.getDescription()
+            tvUsername.text = post.getUser()?.username
+
+            Glide.with(itemView.context)
+                .load(post.getPFP())
+                .into(ivPFP)
+        }
+    }
+}
