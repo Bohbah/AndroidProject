c7fbcd05510dfe804ad8ba5a2912b504b5e7e37f
Robert Diasio <rdd43@njit.edu>
2022-11-28 21:16:21 -0500

    other

--- app/src/main/java/com/androidstudioprojects/grapevine/Post.kt
+++ app/src/main/java/com/androidstudioprojects/grapevine/Post.kt
@@ -1,6 +1,7 @@
 package com.androidstudioprojects.grapevine
 
 import com.parse.*
+import java.util.*
 
 
 // Description : String
@@ -16,6 +17,34 @@ class Post : ParseObject() {
         put(KEY_DESCRIPTION, description)
     }
 
+    fun getLocation(): String? {
+        return getString(KEY_LOCATION)
+    }
+    fun setLocation(Location: String) {
+        put(KEY_LOCATION, Location)
+    }
+
+    fun getDateTime(): String? {
+        return getString(KEY_DATETIME).toString()
+    }
+    fun setDateTime(Date_Time: Date) {
+        put(KEY_DATETIME, Date_Time)
+    }
+
+    fun getName(): String? {
+        return getString(KEY_NAME)
+    }
+    fun setName(Name: String) {
+        put(KEY_NAME, Name)
+    }
+
+    fun getEvent(): String? {
+        return getString(KEY_ISEVENT)
+    }
+    fun setEvent(isEvent: Boolean) {
+        put(KEY_NAME, isEvent)
+    }
+
     fun getUser(): ParseUser? {
         return getParseUser(KEY_USER)
     }
@@ -28,5 +57,9 @@ class Post : ParseObject() {
         const val KEY_DESCRIPTION = "Description"
         const val KEY_PFP = "pfp"
         const val KEY_USER = "User"
+        const val KEY_LOCATION = "Location"
+        const val KEY_DATETIME = "Date_Time"
+        const val KEY_NAME = "Name"
+        const val KEY_ISEVENT = "isEvent"
     }
 }
