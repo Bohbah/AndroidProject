5c0399f9f2b2305b88c19d4c91c794afaeb7d325
mflynn19 <meghan.flynn@theflynn.org>
2022-11-29 22:18:58 -0500

    [Meghan Flynn] home feed onclick listener implemented

--- app/src/main/java/com/androidstudioprojects/grapevine/PostAdapter.kt
+++ app/src/main/java/com/androidstudioprojects/grapevine/PostAdapter.kt
@@ -1,13 +1,18 @@
 package com.androidstudioprojects.grapevine
 
 import android.content.Context
+import android.content.Intent
+import android.graphics.Movie
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
+import com.androidstudioprojects.grapevine.fragments.ProfileFragment
 import com.bumptech.glide.Glide
+import org.parceler.Parcels
+
 
 class PostAdapter(val context: Context, val posts: List<Post>)
     : RecyclerView.Adapter<PostAdapter.ViewHolder>() {
@@ -26,7 +31,7 @@ class PostAdapter(val context: Context, val posts: List<Post>)
         return posts.size
     }
 
-    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView), View.OnClickListener {
         val ivPFP: de.hdodenhof.circleimageview.CircleImageView
         //TODO Org
         val tvUsername: TextView
@@ -37,6 +42,7 @@ class PostAdapter(val context: Context, val posts: List<Post>)
             //TODO Org
             tvUsername = itemView.findViewById(R.id.tvUsername)
             tvDescription = itemView.findViewById(R.id.tvDescription)
+            itemView.setOnClickListener(this)
         }
 
         fun bind(post: Post) {
@@ -46,6 +52,15 @@ class PostAdapter(val context: Context, val posts: List<Post>)
             Glide.with(itemView.context)
                 .load(post.getUser()?.get("pfp"))
                 .into(ivPFP)
+        }
+
+        override fun onClick(p0: View?) {
+                val post = posts[adapterPosition]
+                Toast.makeText(context, "click", Toast.LENGTH_SHORT).show()
+                //val intent = Intent(context, ProfileFragment::class.java)
+                //intent.putExtra("username", tvUsername)
+                //context.startActivity(intent)
+
         }
     }
 }
