18f3169e14bb17d9806c4dab9944426db0f57fc3
Robert Diasio <rdd43@njit.edu>
2022-12-07 19:54:01 -0500

    Added front end files for the chat, the incoming and outgoing messages, and the functionality to send messages to the server.

--- app/src/main/java/com/androidstudioprojects/grapevine/MainActivity.kt
+++ app/src/main/java/com/androidstudioprojects/grapevine/MainActivity.kt
@@ -1,19 +1,19 @@
 package com.androidstudioprojects.grapevine
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
-import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import com.androidstudioprojects.grapevine.fragments.*
 import com.google.android.material.bottomnavigation.BottomNavigationView
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_main)
-
         /*
         Bottom Nav Bar navigation code
          */
--- app/src/main/java/com/androidstudioprojects/grapevine/fragments/ChatFragment.kt
+++ app/src/main/java/com/androidstudioprojects/grapevine/fragments/ChatFragment.kt
@@ -1,11 +1,20 @@
 package com.androidstudioprojects.grapevine.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.ImageButton
+import android.widget.Toast
+import androidx.fragment.app.Fragment
 import com.androidstudioprojects.grapevine.R
+import com.parse.ParseException
+import com.parse.ParseObject
+import com.parse.ParseUser
+import com.parse.SaveCallback
+
 
 class ChatFragment : Fragment() {
     override fun onCreateView(
@@ -19,5 +28,27 @@ class ChatFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         // Set onClickListeners and logic
+        val USER_SEND_ID_KEY = "sender"
+        val BODY_KEY = "content"
+        // Find the text field and button
+        var etMessage = view.findViewById<EditText>(R.id.etMessage)
+        var ibSend = view.findViewById<ImageButton>(R.id.ibSend)
+
+        ibSend.setOnClickListener {
+            val data: String = etMessage.text.toString()
+            val message = ParseObject.create("message")
+            message.put(USER_SEND_ID_KEY, ParseUser.getCurrentUser())
+            message.put(BODY_KEY, data)
+            message.saveInBackground(object : SaveCallback {
+                override fun done(e: ParseException?) {
+                    if (e == null) {
+                        Toast.makeText(activity, "Successfully created message on Parse", Toast.LENGTH_SHORT).show()
+                    } else {
+                        Log.e("ROB", "Failed to save message", e)
+                    }
+                }
+            })
+            etMessage.text = null
+        }
     }
 }
